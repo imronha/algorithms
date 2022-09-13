4a06a3cdca80482f728117384fad29cf9d007fb2
imronha <imronha@gmail.com>
2022-09-12 20:40:45 -0700

    Update Example1.js

--- Udemy DS and Algos/Example1.js
+++ Udemy DS and Algos/Example1.js
@@ -1,5 +1,5 @@
 /*
-
+https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/learn/lecture/12214986#overview
 
 1. Write down the key points at the top
 
@@ -42,10 +42,14 @@ containsCommonItem()
 
 if array sizes are the same, big O would be O(n^2), if they are different big O would be O(a*b)
 
+11. How can i break this code?
+Change the inputs to something unexpected, duplicate items in array, change type of array item (to num)
+Forgetting one of the inputs
 
 */
 
 
+//=================================================================================
 
 
 
@@ -66,3 +70,64 @@ function containsCommonItem (arr1, arr2) {
 }
 
 containsCommonItem(array1, array2)
+
+// Time complexity:
+// O(a*b). Big O is dependent on size of array1 x size of array2 (nested steps are multiplied)
+// O(n^2). Big O is On^2 if the arrays are the same size
+
+// Space complexity:
+// O(1). Constant because we are not using any extra space (no extra variables)
+
+//=================================================================================
+
+
+// Optimized solution pseudocode
+/*
+
+Loop through the first array and create an object where properties = items in the array
+Loop throught he 2nd array and check if a property alreayd exists in the object
+Return true if found, false if not
+
+// Time complexity:
+// O(a+b). Big O is dependent on size of array1 + size of array2 (steps are added if theyre not nested)
+
+// Space complexity:
+// O(a). Constant because we are not using any extra space (no extra variables)
+
+*/
+
+function containsCommonItem2 (arr1, arr2) {
+    let map = {};
+
+    // Loop through the first array and create an object where properties = items in the array
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[i]) {
+            const item = arr1[i]
+            map[item] = true;
+        }
+    }
+    // Loop throught he 2nd array and check if a property alreayd exists in the object
+    for (let j = 0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+//=================================================================================
+
+
+// Optimized solution 2 pseudocode 
+/*
+
+*/
+
+function containsCommonItem3(arr1, arr2) {
+    return arr1.some(item => arr2.includes(item))
+}
+
+containsCommonItem3(array1, array2)
