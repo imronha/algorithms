a6a256d38bf93c2d36bd06a646b8144cce3c107f
imronha <imronha@gmail.com>
2022-11-10 21:12:15 -0800

    Update LinkedListReversal.js

--- Udemy DS and Algos/LinkedListReversal.js
+++ Udemy DS and Algos/LinkedListReversal.js
@@ -166,3 +166,22 @@ Output: []
 
 */
 
+
+/*
+function reverseLinkedList(head){
+    let previous = null;
+    let current = head;
+    let next = null;
+
+    while(current != null){
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+
+    head = previous;
+    return head;
+}
+
+*/
